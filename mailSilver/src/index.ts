f8d1cca49f0da0c345718a42d185d56fccc0a6af
kilokite <kilokiter@outlook.com>
2025-12-20 18:06:13 +0800

    22

--- mailSilver/src/index.ts
+++ mailSilver/src/index.ts
@@ -4,7 +4,7 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('Hello Hono welcome to mailSilver!')
 })
 let info = {};
 app.get('/web_hook', (c) => {
